47918d39842fca9d96a673cbc140c57481839225
pasecolra <paulcolmenares@colegioguanenta.edu.co>
2023-04-26 09:04:41 -0500

    se agrega los cambios

---
+++ desktop_apps_01.py
@@ -0,0 +1,23 @@
+#-----------------------------
+#--------desktop NO.1---------
+#-----------------------------
+
+# se importa toda la libreria tkinter con todas sus funciones
+
+from tkinter import *
+
+#------------------
+# funcion de la app
+#------------------
+
+#-----------------------------
+# pantalla principal de la app 
+#-----------------------------
+
+# se declara una variable llamada ventana_principal que adquiera las caracteristicas de un objeto Tk()
+
+ventana_principal= Tk()
+
+# run
+
+ventana_principal.mainloop()
