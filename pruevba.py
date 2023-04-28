2721791fbcbfdd27779dd55dcb1939f85da00706
pasecolra <paulcolmenares@colegioguanenta.edu.co>
2023-04-28 07:01:32 -0500

    se agregan los cambios

--- pruevba.py
+++ pruevba.py
@@ -10,7 +10,7 @@ ventana_principal.geometry("500x300")
 
 frame_ventana=Frame(ventana_principal)
 
-frame_ventana.config(bg="dark orange", width=500,height=300)
+frame_ventana.config(bg="dark orange", width=200,height=300)
 
 frame_ventana.place(x=0,y=0)
 
